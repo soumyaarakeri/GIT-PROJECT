4d84c108ae45fefa99fde79ea5574e1b50250621
soumyarangapur <soumyarangapura@gmail.com>
2024-03-04 21:11:50 +0530

    taking user input

---
+++ project1.java
@@ -0,0 +1,8 @@
+Scanner sc=new Scanner(Sytem.in);
+System.out.println("Enter the number of units for calculaating electricity bill:");
+units=sc.nextInt();
+if(units<100)
+{
+billTopay=units*1.20;
+}
+
